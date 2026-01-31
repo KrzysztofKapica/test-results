1081f362cd5c3fab0debf78b0e5872f6f72d096f
kacper-echaust <k9echaust@gmail.com>
2026-01-31 10:23:39 +0100

    create newsletter section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AmbassadorsSection } from "./Components/AmbassadorsSection";
 import { HabitSection } from "./Components/HabitSection";
+import { NewsletterSection } from "./Components/Newsletter/NewsletterSection";
 import { PatronitesSections } from "./Components/PatronitesSection";
 
 const App = () => {
   return (
     <>
       <HabitSection />
+      <NewsletterSection />
       <AmbassadorsSection />
       <PatronitesSections />
     </>
