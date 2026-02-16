6104e253333e06052f941a823c6f43f1563b0f34
kacper-echaust <k9echaust@gmail.com>
2026-02-16 20:00:16 +0100

    add hover

--- src/Components/StartSection.tsx
+++ src/Components/StartSection.tsx
@@ -1,27 +1,28 @@
 const StartSection = () => {
   return (
     <section className="py-16 bg-white relative before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-[url('/backgrounds/straight-lines.svg')] before:bg-[length:500px] before:bg-repeat before:opacity-30">
-        <div className="mt-5 ml-8 md:ml-12 lg:ml-[75px]">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-            <span className="text-brandGraphite">Masz wpływ</span>
-            <br />
-            <span className="text-brandGraphite">na twoje </span>
-            <span className="text-brandBlue">szczęście</span>
-          </h2>
+      <div className="mt-5 ml-8 md:ml-12 lg:ml-[75px]">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+          <span className="text-brandGraphite">Masz wpływ</span>
+          <br />
+          <span className="text-brandGraphite">na twoje </span>
+          <span className="text-brandBlue">szczęście</span>
+        </h2>
 
-          <p className="text-xl md:text-2xl font-bold mb-8">
-            <span className="text-brandGraphite">Zrób to </span>
-            <span className="text-brandBlue">#MałymiKrokami.</span>
-          </p>
+        <p className="text-xl md:text-2xl font-bold mb-8">
+          <span className="text-brandGraphite">Zrób to </span>
+          <span className="text-brandBlue">#MałymiKrokami.</span>
+        </p>
 
-          <a
-            href="#TODO"
-            className="bg-brandYellow text-black font-bold text-lg px-4 py-2 rounded-full border-2 border-black hover:opacity-80 transition-colors text-sm -ml-4 shadow-lg"
-            // plus cien i w lewo - zjustowac tekst, ale ramka moze byc w lewo
-          >
-            Dołącz do inicjatywy!
-          </a>
-          <div className="h-[100px]"></div>
+        <a
+          href="#TODO"
+          className="relative z-[999] pointer-events-auto bg-brandYellow text-black font-bold text-lg px-4 py-2 rounded-full border-2 border-black shadow-lg transition-all duration-300 -ml-4 hover:bg-black hover:text-white"
+
+          // plus cien i w lewo - zjustowac tekst, ale ramka moze byc w lewo
+        >
+          Dołącz do inicjatywy!
+        </a>
+        <div className="h-[100px]"></div>
       </div>
     </section>
   );
